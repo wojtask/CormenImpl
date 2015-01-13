4246477898a61cb37d478d83b8bdd55c5fb12014
Krzysztof Wojtas <kwojtas@gmail.com>
2015-01-13 02:09:17 +0100

    permute by sorting + test, randomize in place + test, randomize in place' + test, permute uniformly by sorting + test, random search + test

--- src/main/java/pl/kwojtas/cormenimpl/Chapter5.java
+++ src/main/java/pl/kwojtas/cormenimpl/Chapter5.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class Chapter5 {
 
-    private Chapter5() { }
+    private static Random rand = new Random(System.currentTimeMillis());
+    private static double probabilityForBiasedRandom = rand.nextDouble();
 
-    private static double probabilityForBiasedRandom = new Random(System.currentTimeMillis()).nextDouble();
+    private Chapter5() { }
 
     // solution of 5.1-2
     public static int random(int a, int b) {
@@ -33,8 +34,153 @@ public class Chapter5 {
 
     // solution of 5.1-3
     private static int biasedRandom() {
-        Random rand = new Random(System.currentTimeMillis());
         return rand.nextDouble() <= probabilityForBiasedRandom ? 1 : 0;
+    }
+
+    // subchapter 5.3
+    public static <T> Array<T> permuteBySorting(Array<T> A) {
+        int n = A.length;
+        Array<Integer> P = new Array<>();
+        for (int i = 1; i <= n; i++) {
+            P.set(i, random(1, n * n * n));
+        }
+        sortUsingExternalKeys(A, P, 1, n);
+        return A;
+    }
+
+    // subchapter 5.3
+    private static <T> void sortUsingExternalKeys(Array<T> A, Array<Integer> keys, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            sortUsingExternalKeys(A, keys, p, q);
+            sortUsingExternalKeys(A, keys, q + 1, r);
+            mergeUsingExternalKeys(A, keys, p, q, r);
+        }
+    }
+
+    // subchapter 5.3
+    private static <T> void mergeUsingExternalKeys(Array<T> A, Array<Integer> keys, int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+        Array<T> L = new Array<>();
+        Array<T> R = new Array<>();
+        Array<Integer> keysL = new Array<>();
+        Array<Integer> keysR = new Array<>();
+        for (int i = 1; i <= n1; i++) {
+            L.set(i, A.at(p + i - 1));
+            keysL.set(i, keys.at(p + i - 1));
+        }
+        for (int j = 1; j <= n2; j++) {
+            R.set(j, A.at(q + j));
+            keysR.set(j, keys.at(q + j));
+        }
+        keysL.set(n1 + 1, Integer.MAX_VALUE);
+        keysR.set(n2 + 1, Integer.MAX_VALUE);
+        int i = 1;
+        int j = 1;
+        for (int k = p; k <= r; k++) {
+            if (keysL.at(i) <= keysR.at(j)) {
+                A.set(k, L.at(i));
+                keys.set(k, keysL.at(i));
+                i++;
+            } else {
+                A.set(k, R.at(j));
+                keys.set(k, keysR.at(j));
+                j++;
+            }
+        }
+    }
+
+    // subchapter 5.3
+    public static <T> void randomizeInPlace(Array<T> A) {
+        int n = A.length;
+        for (int i = 1; i <= n; i++) {
+            A.exch(i, random(i, n));
+        }
+    }
+
+    // solution of 5.3-1
+    public static <T> void randomizeInPlace_(Array<T> A) {
+        int n = A.length;
+        A.exch(1, random(1, n));
+        for (int i = 2; i <= n; i++) {
+            A.exch(i, random(i, n));
+        }
+    }
+
+    // solution of 5.3-6
+    public static <T> Array<T> permuteUniformlyBySorting(Array<T> A) {
+        int n = A.length;
+        Array<Integer> P = new Array<>();
+        for (int i = 1; i <= n; i++) {
+            P.set(i, random(1, n * n * n));
+        }
+        sortUniformlyUsingExternalKeys(A, P, 1, n);
+        return A;
+    }
+
+    // solution of 5.3-6
+    private static <T> void sortUniformlyUsingExternalKeys(Array<T> A, Array<Integer> keys, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            sortUniformlyUsingExternalKeys(A, keys, p, q);
+            sortUniformlyUsingExternalKeys(A, keys, q + 1, r);
+            mergeUniformlyUsingExternalKeys(A, keys, p, q, r);
+        }
+    }
+
+    // solution of 5.3-6
+    private static <T> void mergeUniformlyUsingExternalKeys(Array<T> A, Array<Integer> keys, int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+        Array<T> L = new Array<>();
+        Array<T> R = new Array<>();
+        Array<Integer> keysL = new Array<>();
+        Array<Integer> keysR = new Array<>();
+        for (int i = 1; i <= n1; i++) {
+            L.set(i, A.at(p + i - 1));
+            keysL.set(i, keys.at(p + i - 1));
+        }
+        for (int j = 1; j <= n2; j++) {
+            R.set(j, A.at(q + j));
+            keysR.set(j, keys.at(q + j));
+        }
+        keysL.set(n1 + 1, Integer.MAX_VALUE);
+        keysR.set(n2 + 1, Integer.MAX_VALUE);
+        int i = 1;
+        int j = 1;
+        for (int k = p; k <= r; k++) {
+            if (keysL.at(i) < keysR.at(j) || (keysL.at(i).equals(keysR.at(j)) && Util.random() == 0)) {
+                A.set(k, L.at(i));
+                keys.set(k, keysL.at(i));
+                i++;
+            } else {
+                A.set(k, R.at(j));
+                keys.set(k, keysR.at(j));
+                j++;
+            }
+        }
+    }
+
+    // solution of 5-2(a)
+    public static <T> Integer randomSearch(Array<T> A, T x) {
+        int n = A.length;
+        Array<Boolean> B = new Array<>();
+        for (int k = 1; k <= n; k++) {
+            B.set(k, false);
+        }
+        int checked = 0;
+        while (checked < n) {
+            int i = random(1, n);
+            if (A.at(i).equals(x)) {
+                return i;
+            }
+            if (!B.at(i)) {
+                B.set(i, true);
+                checked++;
+            }
+        }
+        return null;
     }
 
 }
--- src/test/java/pl/kwojtas/cormenimpl/Chapter5Test.java
+++ src/test/java/pl/kwojtas/cormenimpl/Chapter5Test.java
@@ -6,7 +6,10 @@ import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static pl.kwojtas.cormenimpl.TestUtil.assertArrayEquals;
 
 @RunWith(DataProviderRunner.class)
 public class Chapter5Test {
@@ -34,6 +37,128 @@ public class Chapter5Test {
 
         // then
         assertTrue(a <= pickedNumber && pickedNumber <= b);
+    }
+
+    @DataProvider
+    public static Object[][] provideDataForPermuting() {
+        return new Object[][]{
+                {new Array<>(34)},
+                {new Array<>(1,2,3)},
+                {new Array<>(9,8,8,7,7,6,6,6,5,3,2,1)},
+                {new Array<>(3.14,-2.75,-0.53,2.55,2.23)},
+                {new Array<>("aaa","eee","ccc","ddd","bbb")}
+        };
+    }
+
+    @Test
+    @UseDataProvider("provideDataForPermuting")
+    public <T extends Comparable> void shouldPermuteBySorting(Array<T> array) {
+        // given
+        Array<T> original = new Array<>(array);
+
+        // when
+        Chapter5.permuteBySorting(array);
+
+        // then
+        assertShuffled(original, array);
+    }
+
+    @Test
+    @UseDataProvider("provideDataForPermuting")
+    public <T extends Comparable> void shouldRandomizeInPlace(Array<T> array) {
+        // given
+        Array<T> original = new Array<>(array);
+
+        // when
+        Chapter5.randomizeInPlace(array);
+
+        // then
+        assertShuffled(original, array);
+    }
+
+    @Test
+    @UseDataProvider("provideDataForPermuting")
+    public <T extends Comparable> void shouldRandomizeInPlaceUsingAlternativeMethod(Array<T> array) {
+        // given
+        Array<T> original = new Array<>(array);
+
+        // when
+        Chapter5.randomizeInPlace_(array);
+
+        // then
+        assertShuffled(original, array);
+    }
+
+    @Test
+    @UseDataProvider("provideDataForPermuting")
+    public <T extends Comparable> void shouldPermuteUniformlyBySorting(Array<T> array) {
+        // given
+        Array<T> original = new Array<>(array);
+
+        // when
+        Chapter5.permuteUniformlyBySorting(array);
+
+        // then
+        assertShuffled(original, array);
+    }
+
+    @DataProvider
+    public static Object[][] provideDataForSuccessfulRandomSearch() {
+        return new Object[][]{
+                {new Array<>(34), 34, new Array<>(1)},
+                {new Array<>(5,7,9,2,6,1,6,6,3,1,7,8), 6, new Array<>(5,7,8)},
+                {new Array<>(5,7,9,2,6,1,6,6,3,1,7,8), 8, new Array<>(12)},
+                {new Array<>(5.0,-2.3,-1.3,-1.9,-2.3), -2.3, new Array<>(2,5)},
+                {new Array<>("aaa","bbb","aaa","ccc"), "ccc", new Array<>(4)}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] provideDataForUnsuccessfulRandomSearch() {
+        return new Object[][]{
+                {new Array<>(34), 35},
+                {new Array<>(5,7,9,2,6,1,6,6,3,1,7,8), 4},
+                {new Array<>(5.0,-2.3,-1.3,-1.9,-2.3), 2.3},
+                {new Array<>("aaa","bbb","aaa","ccc"), "xyz"}
+        };
+    }
+
+    @Test
+    @UseDataProvider("provideDataForSuccessfulRandomSearch")
+    public <T> void shouldFindObject(Array<T> array, T key, Array<Integer> expectedIndexes) {
+        // given
+
+        // when
+        Integer actualIndex = Chapter5.randomSearch(array, key);
+
+        // then
+        assertArrayContains(expectedIndexes, actualIndex);
+    }
+
+    @Test
+    @UseDataProvider("provideDataForUnsuccessfulRandomSearch")
+    public <T> void shouldNotFindObject(Array<T> array, T key) {
+        // given
+
+        // when
+        Integer actualIndex = Chapter5.randomSearch(array, key);
+
+        // then
+        assertNull(actualIndex);
+    }
+
+    private static <T extends Comparable> void assertShuffled(Array<T> original, Array<T> shuffled) {
+        Chapter2.mergeSort_(original, 1, original.length);
+        Chapter2.mergeSort_(shuffled, 1, shuffled.length);
+        assertArrayEquals(original, shuffled);
+    }
+
+    private static <T> void assertArrayContains(Array<T> array, T element) {
+        boolean contains = false;
+        for (int i = 1; i <= array.length; i++) {
+            contains = contains || array.at(i).equals(element);
+        }
+        assertTrue(contains);
     }
 
 }
